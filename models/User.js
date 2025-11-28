ad9f4108f340898efbee9abc087ec7e21821d1d7
Nandhini-priya-r <nandhini120904@gmail.com>
2025-11-28 19:28:57 +0530

    BE changes 1

--- models/User.js
+++ models/User.js
@@ -11,7 +11,6 @@ const userSchema = new mongoose.Schema({
     cartItems: {type: Object, default: {}}
 
 },{  minimize: false})
-
-const User = mongoose.model.user || mongoose.model('user',userSchema)
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User
