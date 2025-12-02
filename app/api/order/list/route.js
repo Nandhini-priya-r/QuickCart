055f507e92178ecc679ece67d549581a28b2dadc
Nandhini-priya-r <nandhini120904@gmail.com>
2025-12-02 18:15:33 +0530

    finial commit

---
+++ app/api/order/list/route.js
@@ -0,0 +1,35 @@
+import connectDB from "@/config/db";
+import Order from "@/models/Order";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
+
+    if (!userId) {
+      return NextResponse.json({
+        success: false,
+        message: "User is not authenticated",
+      });
+    }
+
+    await connectDB();
+
+    // Fetch all orders for this user
+    const orders = await Order.find({ userId })
+      .populate("address")
+      .populate("items.product");
+
+    return NextResponse.json({
+      success: true,
+      orders,
+    });
+
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
